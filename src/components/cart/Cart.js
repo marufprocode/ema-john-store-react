32a55e6e3771a4f15330b392b86e42c2d9c4b04d
unknown <maruf.procode@gmail.com>
2022-09-26 15:41:21 +0600

    cart updated

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cart = ({cartItem}) => {
     return (
         <div>
-            <h3>Order Summary</h3>
+            <h3>Order Summary: </h3>
             <h4>Order Items: {cartItem.length}</h4>
         </div>
     );
